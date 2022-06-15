62297695596c2b6a5d0c4f68430259709b37244b
Anastasiia <seliverstova.anastasia3@yandex.ru>
2022-06-15 19:17:44 +0300

    Update DB, create new seeders

--- db/seeders/20220615150802-couriers.js
+++ db/seeders/20220615150802-couriers.js
@@ -3,12 +3,22 @@ const bcrypt = require('bcrypt');
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Couriers', [{
+      id: 1,
       name: 'Бага',
       password: await bcrypt.hash('courier', 6),
       phone: '88652473992',
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
+    },
+    {
+      id: 2,
+      name: 'Маришка',
+      password: await bcrypt.hash('courier', 6),
+      phone: '82152473992',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    ], {});
   },
 
   async down(queryInterface, Sequelize) {
