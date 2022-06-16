134870cf027b9822ff2cbcd8c9cf8b1b9ed457ee
Aleksandr <Aleksandr266@list.ru>
2022-06-16 22:37:15 +0300

    сделал верстку карточек, регистрацию и логин

--- views/templates/Layout.jsx
+++ views/templates/Layout.jsx
@@ -16,6 +16,9 @@ function Layout({ children }) {
           crossOrigin="anonymous"
         />
         <link href="/css/style.css" rel="stylesheet" />
+        <link href="/css/login.css" rel="stylesheet" />
+        <link rel="stylesheet" href="/css/registration.css" />
+        <link rel="stylesheet" href="/css/cards.css" />
       </head>
       <body className="back">
         <header>
@@ -27,7 +30,7 @@ function Layout({ children }) {
           <nav className=" bg-warning w-100 p-1">
             <form className="d-flex ">
               <div className="text-dark flex-grow-1 fw-bold text-decoration-none fs-4">
-                <a type="submit" href="/reg"><img src="/img/logo.png" width="100" alt=" " /></a>
+                <a type="submit" href="/"><img src="/img/logo.png" width="100" alt=" " /></a>
               </div>
               <Navbar />
             </form>
