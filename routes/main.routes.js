601bc642eb95aec166d60c43d7b2fec2198fdf38
Anton Valkovsky <ruizave@gmail.com>
2022-06-16 16:49:54 +0300

    реализован вывод заказов только для пользователя

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,12 +1,14 @@
 const mainRouter = require('express').Router();
 const Main = require('../views/Main');
 const Error = require('../views/templates/Error');
-const { Order } = require('../db/models');
+const { Order, User } = require('../db/models');
 
 mainRouter.get('/', async (req, res) => {
   // если пользователь залогинен, то в хранилище сессии лежит его userId
+  const { userId } = req.session;
+  const user = userId && await User.findByPk(userId);
   const orders = await Order.findAll();
-  res.renderComponent(Main, { orders });
+  res.renderComponent(Main, { user, orders });
 });
 
 mainRouter.get('/error', (req, res) => {
