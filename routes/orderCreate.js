4c975f224185306bc49fcffdb18f06f45c177317
Anastasiia <seliverstova.anastasia3@yandex.ru>
2022-06-16 15:00:57 +0300

    Create orderCreate.route

---
+++ routes/orderCreate.js
@@ -0,0 +1,8 @@
+require('dotenv');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const multer = require('multer');
+const orderCreate = require('../views/orders/OrderCreate');
+const takePic = require('../middlewares/takePic');
+
+const upload = multer({ storage });
