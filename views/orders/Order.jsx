3e99f82fb2c837b420b719bc2033e5aed2e36442
Aleksandr <Aleksandr266@list.ru>
2022-06-17 16:28:50 +0300

    ..

--- views/orders/Order.jsx
+++ views/orders/Order.jsx
@@ -15,8 +15,8 @@ module.exports = function Order({ order }) {
         <div className="deckription">
           <p className="orderDescription fw-light fs-6">{description}</p>
         </div>
-        <p className="price fw-light fs-6">Обычная цена: {price} $</p>
-        <p className="salePrice">Моя цена: {salePrice} $</p>
+        <p className="price fw-light fs-6">Обычная цена: {price} ₽</p>
+        <p className="salePrice">Моя цена: {salePrice} ₽</p>
       </div>
       <form action={`/api/orders/${id}/accept`} method="POST" className="order">
         <button className="accept btn btn-outline-dark" type="submit">Заказать</button>
