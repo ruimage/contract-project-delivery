4ae87e9dc9acd71e872e4409ae54b85b81d5257f
Anton Valkovsky <ruizave@gmail.com>
2022-06-17 11:35:11 +0300

    Исправлена панель на профиле пользователя

--- views/profile/Profile.jsx
+++ views/profile/Profile.jsx
@@ -2,19 +2,10 @@ const React = require('react');
 const Layout = require('../templates/Layout');
 const NavBar = require('../NavBar');
 
-module.exports = function Main({ user, orders }) {
+module.exports = function Profile({ user }) {
   return (
     <Layout>
-      <nav className=" bg-warning w-100 p-2">
-        <form className="d-flex ">
-          <div className="text-dark flex-grow-1 fw-bold text-decoration-none fs-2">
-            <a type="submit" href="/reg">
-              <img src="/img/logo.png" width="100" alt=" " />
-            </a>
-          </div>
-          <NavBar user={user} />
-        </form>
-      </nav>
+      <NavBar user={user} />
 
       <div className="profile">{`Имя  ${user.firstName}`}</div>
       <div className="profile">{`Фамилия ${user.lastName}`}</div>
